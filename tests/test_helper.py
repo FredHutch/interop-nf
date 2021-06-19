1d620f5d1aa5f3bdcf3fb6829bcbc1f7b0b69929
Nathan Thorpe <nathan@techstormpc.com>
2021-06-18 23:02:44 -0700

    Add automated tests for InterOp parsing with MiSeq demo dataset (#3)
    * add unit and integration tests
    
    * add to github actions pipeline
    
    * fix bad module path, add assert for file
    
    * change zip output dir
    
    * add column def into run summary file
    
    * change demo path
    
    * list output directory
    
    * remove init file

---
+++ tests/test_helper.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+tests_dir = os.path.dirname(__file__)
+
+
+def get_resource_path(path: str):
+    return str(Path(os.path.join(tests_dir, path)))
+
+
+miseq_demo_path = get_resource_path('data/MiSeqDemo/')
