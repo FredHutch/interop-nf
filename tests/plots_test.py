46796ce991e91e18d217ae456b8c7df78b8ce54d
Nathan Thorpe <nathan@techstormpc.com>
2021-06-19 09:57:50 -0700

    add code coverage report

--- tests/plots_test.py
+++ tests/plots_test.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 
+from bin.plot_occupancy import plot_occupancy
 from bin.plot_percent_base import plot_percent_base
 from bin.plot_tile_intensity import plot_tile_intensity
 from tests.test_helper import miseq_demo_path
@@ -19,3 +20,11 @@ class PlotTests(unittest.TestCase):
         plt.figure()
         plot_tile_intensity(miseq_demo_path)
         self.assertTrue(Path('max_intensity_1.svg').exists())
+
+    def test_plot_occupancy_should_not_created_on_demo(self):
+        """ MiSeq shouldn't have this data """
+        plt.figure()
+        plot_occupancy(miseq_demo_path)
+        self.assertFalse(Path('occupancy_tile.jpg').exists())
+        self.assertFalse(Path('occupancy_cycle.jpg').exists())
+        self.assertFalse(Path('occupancy_read.jpg').exists())
